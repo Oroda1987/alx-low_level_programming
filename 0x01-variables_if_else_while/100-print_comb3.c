83ea4b4631a9530c51e8a2f7e27e92074e5f5d54
oroda1987 <orodairene4@gmail.com>
2023-07-17 09:28:17 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,15 +12,12 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0 + 1; b <= 9; b++)
+		for (b = a + 1; b <= 9; b++)
 		{
-			if (a != b)
-			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
