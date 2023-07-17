692b9b5cf2591c68198099d4abfca6cf1b68341e
oroda1987 <orodairene4@gmail.com>
2023-07-17 11:08:58 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,21 +11,21 @@ int main(void)
 	int e;
 	int f;
 
-	for (d = 0; d <= 9; d++)
+	for (d = 0; d < 8; d++)
 	{
-		for (e = d + 1; e <= 9; e++)
+		for (e = d + 1; e < 9; e++)
 		{
-			for (f = e + 1; f <= 9; f++)
+			for (f = e + 1; f < 10; f++)
 			{
-				if (d == 8 && e == 9 && f == 0)
-				break;
-					putchar(d + '0');
-					putchar(e + '0');
-					putchar(f + '0');
+				putchar(d + '0');
+				putchar(e + '0');
+				putchar(f + '0');
+					if (d == 7 && e == 8 && f == 9)
+					continue;
+
 					putchar(',');
 					putchar(' ');
 			}
-			
 		}
 	}
 	putchar('\n');
