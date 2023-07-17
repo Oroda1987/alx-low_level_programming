2a441543e39bc8a183cef1d08d512408772b4f8d
oroda1987 <orodairene4@gmail.com>
2023-07-17 04:52:34 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Enrty point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		putchar(d + '0');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}	
