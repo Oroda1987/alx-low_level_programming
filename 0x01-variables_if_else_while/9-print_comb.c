87fcf5750de15913b9d92ba66d9ad42d1df571bd
oroda1987 <orodairene4@gmail.com>
2023-07-17 07:25:06 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,13 @@ int main(void)
 	for (d = 0; d < 10; d++)
 	{
 		putchar(d + '0');
-		putchar(',');
-		putchar(' ');
+
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
 	}
 	return (0);
 }
