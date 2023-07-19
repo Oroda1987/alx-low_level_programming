d1b3055935cea4cfbf8d98c6790ab16a3d183432
oroda1987 <orodairene4@gmail.com>
2023-07-18 17:48:25 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	void print_alphabet(void);
+
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+		putchar(b);
+		putchar('\n');
+	return (0);
+}	
