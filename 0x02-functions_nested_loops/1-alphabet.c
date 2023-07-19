aa3d2c1870efec3d554fb5a0561b452a75d4dbfd
oroda1987 <orodairene4@gmail.com>
2023-07-18 17:58:28 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,16 @@
  * Return: Always 0 (Success)
  */
 
+void print_alphabet(void);
+
 int main(void)
 {
-	void print_alphabet(void);
-
 	char b;
 
 	for (b = 'a'; b <= 'z'; b++)
+	{
 		putchar(b);
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }	
