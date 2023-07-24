668bdca92ca03d78a93ad44bb3ced29f7e6195ed
oroda1987 <orodairene4@gmail.com>
2023-07-24 05:15:45 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reset_to_98 - takes a pointer to int and updates  it points to to 98
+ *@n: Paremeter to be chaked
+ *Returns: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
