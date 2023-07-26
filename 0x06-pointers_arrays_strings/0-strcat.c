571b3a6eb2151cb992ccd5348d8462fa874dfe9f
oroda1987 <orodairene4@gmail.com>
2023-07-26 12:23:08 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: Destination of the string
+ *@src: Source of the string
+ *Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+
+}
