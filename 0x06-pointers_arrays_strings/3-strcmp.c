455997af9a6474551fdb7ec72656438f6c30fd04
oroda1987 <orodairene4@gmail.com>
2023-07-27 03:02:38 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,8 +12,12 @@ int _strcmp(char *s1, char *s2)
 	/*Compare the characters until we reach the end of s1 or s2 */
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
 		if (s1[i] != s2[i])
-				return (s1[i] - s2[i]);
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
 
 	  /* If both strings are equal, return 0 */
 		return (0);
