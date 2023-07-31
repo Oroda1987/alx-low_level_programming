fb30ffa37a5c0715b733f7ed83d2fda9f8e72b85
oroda1987 <orodairene4@gmail.com>
2023-07-31 07:40:09 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+*_memcpy- Copies memory area
+*@dest:Destination
+*@src:Source 
+*@n:Perameter to be copied
+*Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+
+
+}
