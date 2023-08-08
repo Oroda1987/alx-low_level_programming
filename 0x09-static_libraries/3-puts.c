c35c2a75afd5c39be786221d0c249a3ac05c95c5
oroda1987 <orodairene4@gmail.com>
2023-08-08 11:43:18 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *_puts - build a multilingual puzzle
+ *
+ *@s: Perameter to be checked
+ * Return: nothing
+ */
+
+void _puts(char *s)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
