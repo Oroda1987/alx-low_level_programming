0cae207f4dc9e367d837fe688b0fd137b9c0b662
oroda1987 <orodairene4@gmail.com>
2023-08-14 06:50:08 -0700

    dog.h

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,14 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+#include <stdio.h>
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+struct dog
+
+#endif
