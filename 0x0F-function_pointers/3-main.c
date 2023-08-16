683702b0eb6444f01112c22953554cf8f5a91896
oroda1987 <orodairene4@gmail.com>
2023-08-16 10:37:10 -0700

    899

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ *
+ *
+ *
+ *
+ */
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-cal.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ *op_add - add two numbers
+ *@a: first number
+ *@b: second number
+ *Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - subtracts two numbers
+ *@a:first number
+ *@b: second number
+ *Return: diffrence of aand b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiplies two numbers
+ *@a:first number
+ *@b:second number
+ *Return: poduct of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divide two numbers
+ *@a:first number
+ *@b:second number
+ *Return: quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - multiplies two  numbers
+ *@a:first number
+ *@b:second number
+ *Return: remainder of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
