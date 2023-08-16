a3c6d117adbfa5b2bb16f251c2e4bbd5ad0b48a6
oroda1987 <orodairene4@gmail.com>
2023-08-16 03:10:03 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *print_name - print name from function pointer
+ *@name: char string
+ *@f: function pointer tanking arguments from string
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
