9c03580873d912416026a0f5fe6dfd526b96c2dc
Oroda1987 <orodairene4@gmail.com>
2023-08-26 15:45:21 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,7 +10,7 @@
 int main(int argc, char **argv)
 {
 	int num, j, result = 0;
-	int coins[] = {25, 10, 5, 2, 1};
+	int coins[] = {25, 10, 5, 2};
 
 	if (argc != 2)
 	{
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
 		while (num >= coins[j])
 		{
 			result++;
-			num = coins[j];
+			num += coins[j];
 		}
 	}
 	}
