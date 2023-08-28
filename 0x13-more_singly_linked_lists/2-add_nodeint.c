7de7713aff661024a6b5848c1a468caa6093a35b
Oroda1987 <orodairene4@gmail.com>
2023-08-28 11:21:31 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *add_nodeint - add a node at the  start of the list
+ *@head: head of the list
+ *@n:integer where node to be added
+ *
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+}
