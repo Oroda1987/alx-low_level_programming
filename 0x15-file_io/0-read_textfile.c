043e7af7e485372de8133da4db2789a3b3f5a384
Oroda1987 <orodairene4@gmail.com>
2023-09-05 02:28:10 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd;
 	ssize_t w;
 	ssize_t r;
-	
+
+	if (filename == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
